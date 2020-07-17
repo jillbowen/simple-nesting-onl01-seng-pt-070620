912d2d2b3fe5c2e3e6af83c44daaa7ceb2961509
Jill Short <jillian.b.short@gmail.com>
2020-07-17 23:55:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,5 +1,6 @@
 
 def hopper
+  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -16,8 +17,9 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +39,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages].join("")
 end
 
 def adding_matz
@@ -80,7 +84,10 @@ def adding_matz
         }
      }
 
-    
+programmer_hash << [:yukihiro_matsumoto]
+programmer_hash[:yukihiro_matsumoto]
+
+programmer_hash    
 end
 
 def changing_alan
